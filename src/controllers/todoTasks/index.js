a46fb839f1f52635e83d572223ec1e81428d6562
Raul Barbosa <barbosamartins94@gmail.com>
2022-10-11 19:11:13 -0300

    feature: Endpoint criação de tarefa concluído

--- src/controllers/todoTasks/index.js
+++ src/controllers/todoTasks/index.js
@@ -1,5 +1,41 @@
-const createTask = async (req, res) => {
+const knex = require('../../connection');
 
+const createTask = async (req, res) => {
+    const { id } = req.user;
+    const { description, date_finish } = req.body;
+
+    if (!description || !date_finish) {
+        return res.status(400).json({ message: "All fields need be filled" });
+    }
+
+    try {
+
+        const user = await knex('users').where({ id }).first();
+
+        if (!user) {
+            return res.status(400).json({ message: "User not found." })
+        }
+
+        const task = await
+            knex('tasks')
+                .insert({
+                    description,
+                    date_finish,
+                    user_id: id,
+                    date_creation: new Date()
+                });
+
+        if (task.rowCount === 0) {
+            return res.status(400).json({ message: "Could not create a task." });
+        }
+
+        return res.status(201).json()
+
+
+
+    } catch (error) {
+        return res.status(500).json({ message: error.message })
+    }
 }
 
 const getTask = async (req, res) => {
